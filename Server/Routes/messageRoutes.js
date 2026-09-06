476c6c8b9284861498fb9bf722622ceff4114cb1
N SIVA BRAHMAIAH <23ht1a4374@gmail.com>
2026-09-07 03:10:37 +0530

    Add route for marking messages as seen
    Added route to mark messages as seen.

--- Server/Routes/messageRoutes.js
+++ Server/Routes/messageRoutes.js
@@ -1,18 +1,43 @@
 import express from "express";
 import authMiddleware from "../middlewares/authmiddleware.js";
 import messageUpload from "../middlewares/messageUpload.js";
+
 import {
-	getMessages,
-	getUserById,
-	getConversation,
-	sendMessage,
+  getMessages,
+  getUserById,
+  getConversation,
+  sendMessage,
+  markMessagesSeen,
 } from "../Controlers/messageControllers.js";
 
 const router = express.Router();
 
 router.get("/", authMiddleware, getMessages);
-router.get("/users/:userId", authMiddleware, getUserById);
-router.get("/:userId", authMiddleware, getConversation);
-router.post("/:userId", authMiddleware, messageUpload, sendMessage);
+
+router.get(
+  "/users/:userId",
+  authMiddleware,
+  getUserById
+);
+
+router.get(
+  "/:userId",
+  authMiddleware,
+  getConversation
+);
+
+router.post(
+  "/:userId",
+  authMiddleware,
+  messageUpload,
+  sendMessage
+);
+
+// Mark received messages as seen
+router.put(
+  "/:userId/seen",
+  authMiddleware,
+  markMessagesSeen
+);
 
 export default router;

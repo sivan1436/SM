ee618ef63dc05e19ddb66ee1f076f8317722689c
N SIVA BRAHMAIAH <23ht1a4374@gmail.com>
2026-09-06 20:28:02 +0530

    Change API proxy URL to production server

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:4000',
+      '/api': 'https://scrink.onrender.com',
     },
   },
 })

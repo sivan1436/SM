7243b50128b5c7ddbcb1510656fe335989d1c39a
siva <23ht1a4374@gmail.com>
2026-09-06 17:35:11 +0530

    bug fix : import error has fixed

--- client/src/Components/StoriesBar.jsx
+++ client/src/Components/StoriesBar.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Plus } from "lucide-react";
 import moment from "moment";
 import StoryModel from "./CreateStory";
-import StoryViewer from "./StoryViewer";
+import StoryViewer from "./Storyviewer.jsx";
 
 const getUserId = (user) => user?._id || user?.id || user;
 

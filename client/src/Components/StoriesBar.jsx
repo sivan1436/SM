17cd33aa2007f63b46b60de4ffaee433ec118268
N SIVA BRAHMAIAH <23ht1a4374@gmail.com>
2026-09-06 17:53:06 +0530

    Update StoriesBar.jsx

--- client/src/Components/StoriesBar.jsx
+++ client/src/Components/StoriesBar.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { Plus } from "lucide-react";
 import moment from "moment";
-import StoryModel from "./CreateStory";
-import StoryViewer from "./StoryViewer";
+import StoryModel from "./CreateStory.jsx";
+import StoryViewer from "./StoryViewer.jsx";
 
 const getUserId = (user) => user?._id || user?.id || user;
 
